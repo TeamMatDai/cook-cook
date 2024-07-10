fadb9737e66f5f44e6a764b991262b2ac60233bd
sorakim <sora6418@gmail.com>
2024-07-10 17:24:12 +0900

    feat: 아이콘들 추가 - 뒤로가기, 검색, 공유, 책갈피

--- src/icons/IconBackButton.tsx
+++ src/icons/IconBackButton.tsx
@@ -1,4 +1,4 @@
-export const BackButton = () => (
+export const IconBackButton = () => (
   <svg width="14" height="22" viewBox="0 0 14 22" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       fillRule="evenodd"
---
+++ src/icons/IconBookmark.tsx
@@ -0,0 +1,6 @@
+export const IconBookmark = () => (
+  <svg width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.33341 13L5.00008 9.75L0.666748 13V0H9.33341V13Z" fill="#999999"/>
+  </svg>
+
+)
