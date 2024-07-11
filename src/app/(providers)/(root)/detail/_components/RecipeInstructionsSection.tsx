9fbe1ff3f63b9c160cfc8132bb825a678360b8ba
sorakim <sora6418@gmail.com>
2024-07-12 03:12:34 +0900

    feat: RecipeInstructionsSection 컴포넌트 분리

---
+++ src/app/(providers)/(root)/detail/_components/RecipeInstructionsSection.tsx
@@ -0,0 +1,16 @@
+import { Recipe } from '@/types/recipe';
+import React from 'react';
+
+const RecipeInstructionsSection: React.FC<{ recipe: Recipe}> = ({recipe}) => {
+  return (
+    <article className="pb-4">
+      <h1 className="text-2xl mb-4 text-black font-bold">레시피</h1>
+      <img src={recipe.thumbnail} alt={recipe.title} className="w-full object-cover mb-4" />
+      <div
+        dangerouslySetInnerHTML={{ __html: recipe.recipe }}
+        className="text-base text-gray-600 leading-1.5 mb-4"
+      />
+    </article>
+  )
+}
+export default RecipeInstructionsSection;
