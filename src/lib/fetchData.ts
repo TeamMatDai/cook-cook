da866252209dbd2416691e776fff92c24725b564
sorakim <sora6418@gmail.com>
2024-07-12 03:15:27 +0900

    feat: 북마크 기능 리펙토링으로 인한 api 추가

--- src/lib/fetchData.ts
+++ src/lib/fetchData.ts
@@ -1,16 +1,14 @@
 import { createClient } from '@/utils/supabase/supabaseClient';
+import { Bookmark } from '@/types/bookmark';
 
 const supabase = createClient();
 export const getRecipeById = async (id: string) => {
-  console.log('@@ id', id);
-
   const { data, error } = await supabase.from('recipes').select('*').eq('id', id).single();
 
   if (error) {
     console.error('Error fetching recipe:', error);
     throw error;
   }
-  console.log('Fetched recipe:', data);
   return data;
 };
 
@@ -26,20 +24,41 @@ export const checkBookmark = async (recipesId: string, userId: string) => {
   return data ? true : false;
 };
 
-export const toggleBookmark = async (recipesId: string, userId: string, isBookmarked: boolean) => {
-  if (isBookmarked) {
-    const { error } = await supabase
-      .from('bookmark')
-      .delete()
-      .eq('recipesId', recipesId)
-      .eq('userId', userId);
+export const addBookmark = async (recipesId: string, userId: string) => {
+  const bookmark: Bookmark = { recipesId, userId };
+  const { data, error } = await supabase
+    .from('bookmark')
+    .insert([bookmark]);
 
-    if (error) throw new Error(error.message);
-  } else {
-    const { error } = await supabase
-      .from('bookmark')
-      .insert([{ recipesId, userId }]);
-
-    if (error) throw new Error(error.message);
-  }
+  if (error) throw new Error(error.message);
+  return data;
 };
+
+export const removeBookmark = async (recipesId: string, userId: string) => {
+  const {data, error } = await supabase
+    .from('bookmark')
+    .delete()
+    .eq('recipesId', recipesId)
+    .eq('userId', userId);
+
+  if (error) throw new Error(error.message);
+  return data;
+};
+
+// export const toggleBookmark = async (recipesId: string, userId: string, isBookmarked: boolean) => {
+//   if (isBookmarked) {
+//     const { error } = await supabase
+//       .from('bookmark')
+//       .delete()
+//       .eq('recipesId', recipesId)
+//       .eq('userId', userId);
+//
+//     if (error) throw new Error(error.message);
+//   } else {
+//     const { error } = await supabase
+//       .from('bookmark')
+//       .insert([{ recipesId, userId }]);
+//
+//     if (error) throw new Error(error.message);
+//   }
+// };
