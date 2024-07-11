eea9a35bffaf889aa50ac3253187f03a70816d08
sorakim <sora6418@gmail.com>
2024-07-12 01:13:20 +0900

    fix: layout 컴포넌트 변경 적용

--- src/app/(providers)/(root)/detail/layout.tsx
+++ src/app/(providers)/(root)/detail/layout.tsx
@@ -1,3 +1,11 @@
+import { LAYOUT } from '@/constants/tailwind';
+import BottomNav from '@/app/(providers)/(root)/_components/BottomNav';
+
 export default function DetailLayout({ children }: { children: React.ReactNode }) {
-  return <section className="px-6 pb-[90px] overflow-hidden break-words">{children}</section>;
+  return (
+    <div className={LAYOUT.BOTTOM_NAV_PADDING_BOTTOM}>
+      <section className="pb-[90px] overflow-hidden break-words">{children}</section>
+      <BottomNav />
+    </div>
+  );
 }
