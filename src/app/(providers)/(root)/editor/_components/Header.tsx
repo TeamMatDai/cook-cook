9bcb7a1120b394337d3e3dc07e02c3f926f72bb8
yeseul0809 <dresulsul@naver.com>
2024-07-12 13:28:31 +0900

    refactor:  alias 수정

--- src/app/(providers)/(root)/editor/_components/Header.tsx
+++ src/app/(providers)/(root)/editor/_components/Header.tsx
@@ -1,4 +1,5 @@
 import BackIcon from '@icons/editor/back.svg';
+import Link from 'next/link';
 
 interface HeaderProps {
   onSubmit: () => void;
@@ -6,9 +7,11 @@ interface HeaderProps {
 
 const Header = ({ onSubmit }: HeaderProps) => (
   <header className="flex justify-between items-center border-b border-gray-300 px-6 py-4 gap-5 font-semibold mb-3">
-    <button>
-      <BackIcon />
-    </button>
+    <Link href="/">
+      <button>
+        <BackIcon />
+      </button>
+    </Link>
     <strong>글쓰기</strong>
     <button onClick={onSubmit}>작성</button>
   </header>
--- src/app/(providers)/(root)/editor/_components/MaterialList.tsx
+++ src/app/(providers)/(root)/editor/_components/MaterialList.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import CommonInput from './CommonInput';
 
 interface Material {
