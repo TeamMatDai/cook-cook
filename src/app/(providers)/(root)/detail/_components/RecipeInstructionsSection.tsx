276eb3479aecb1f70e25856ad06399237ed2b1d5
sorakim <sora6418@gmail.com>
2024-07-13 04:21:54 +0900

    fix: next Image 로 이미지 처리 변경

--- src/app/(providers)/(root)/detail/_components/RecipeInstructionsSection.tsx
+++ src/app/(providers)/(root)/detail/_components/RecipeInstructionsSection.tsx
@@ -1,12 +1,12 @@
 import { Recipe } from '@/types/recipe';
 import React from 'react';
+import Image from 'next/image';
 
 const RecipeInstructionsSection: React.FC<{ recipe: Recipe}> = ({recipe}) => {
   return (
     <article className="pb-4">
       <h1 className="text-2xl mb-4 text-black font-bold">레시피</h1>
-      {/*TODO: next image로 변경필요!!!*/}
-      <img src={recipe.thumbnail} alt={recipe.title} className="w-full object-cover mb-4" />
+      <Image src={recipe.thumbnail} alt={recipe.title} width={500} height={500} className="w-full object-cover mb-4" />
       <div
         dangerouslySetInnerHTML={{ __html: recipe.recipe }}
         className="text-base text-gray-600 leading-1.5 mb-4"
--- src/app/(providers)/(root)/detail/_components/UserInfo.tsx
+++ src/app/(providers)/(root)/detail/_components/UserInfo.tsx
@@ -1,18 +1,21 @@
 // TODO: user정보 받아와서 로직 짜기
+import Image from 'next/image';
+
 const UserInfo = () => {
   return (
     <div className="flex flex-col gap-[50px] justify-center items-center">
-      {/*TODO: next image로 변경필요!!!*/}
-      <img
+      <Image
         className="w-[50px] h-[50px] rounded-full overflow-hidden object-cover"
         src="https://scontent-nrt1-2.xx.fbcdn.net/v/t1.6435-9/66059442_2275758249307701_5758914269005479936_n.png?_nc_cat=100&ccb=1-7&_nc_sid=13d280&_nc_ohc=8dsZHav4tcgQ7kNvgEUja4v&_nc_ht=scontent-nrt1-2.xx&oh=00_AYCsT_OzCYO6ORnFoxxnBin-78EpvKcNJZoR7OivDeVBzw&oe=66B59DE5"
-        alt="프로필"
+        alt="프로필이미지"
+        width={50}
+        height={50}
       />
       <div className="flex flex-col gap-2 text-center">
         <div className="font-bold text-[#222222] text-lg">김소라</div>
         <div className="text-base text-[#222222]">소라의 키친</div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default UserInfo;
