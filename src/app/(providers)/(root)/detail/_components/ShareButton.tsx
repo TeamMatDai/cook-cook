0c7fc5165f5aa42009ece33b62eb44a6153158f5
sorakim <sora6418@gmail.com>
2024-07-13 04:23:21 +0900

    fix: handleShare 컴포넌트 함수 밖으로 이동

--- src/app/(providers)/(root)/detail/_components/ShareButton.tsx
+++ src/app/(providers)/(root)/detail/_components/ShareButton.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import { IconShare } from '@/icons/IconShare';
-import { Button } from '@/app/(providers)/(root)/detail/_components/ShareButton';
 
+const handleShare = () => {
+  const url = window.location.href;
+  if (navigator.clipboard) {
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        alert('URL이 클립보드에 복사되었습니다.');
+      })
+      .catch((error) => {
+        alert('error가 발생하였습니다.');
+      });
+  } else {
+    console.log('Clipboard API not supported in this browser.');
+  }
+};
 const ShareButton: React.FC = () => {
-  const handleShare = () => {
-    const url = window.location.href;
-    if (navigator.clipboard) {
-      navigator.clipboard
-        .writeText(url)
-        .then(() => {
-          alert('URL이 클립보드에 복사되었습니다.');
-        })
-        .catch((error) => {
-          console.error('Error copying URL:', error);
-        });
-    } else {
-      console.log('Clipboard API not supported in this browser.');
-    }
-  };
 
   return (
-    <button className='rounded-lg w-[66px] h-[66px] flex flex-col gap-2 justify-center items-center border border-lightgray' onClick={handleShare}>
+    <button type='button' className='rounded-lg w-[66px] h-[66px] flex flex-col gap-2 justify-center items-center border border-lightgray' onClick={handleShare}>
       <IconShare />
       공유
     </button>
