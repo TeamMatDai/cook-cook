cb580d2f14134091e758d161a00f8e8e28f05225
sorakim <sora6418@gmail.com>
2024-07-13 04:20:08 +0900

    fix: 수정사항 반영 - qeury key에 id값 추가, params type 추가

--- src/app/(providers)/(root)/detail/[id]/page.tsx
+++ src/app/(providers)/(root)/detail/[id]/page.tsx
@@ -5,19 +5,24 @@ import { IconBackButton } from '@/icons/IconBackButton';
 import { IconSearch } from '@/icons/IconSearch';
 import { Recipe } from '@/types/recipe';
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
-import RecipeSection from '@/app/(providers)/(root)/detail/_components/RecipeSection';
 import Link from 'next/link';
+import Image from 'next/image';
+import RecipeSection from '@/app/(providers)/(root)/detail/_components/RecipeSection';
 
-const DetailPage: React.FC= () => {
-  const params = useParams();
-  const id = params?.id as string;
+type ParamsType = {
+  id: string;
+};
+
+const DetailPage: React.FC = () => {
+  const params = useParams<ParamsType>();
+  const id = params.id;
 
   const {
     data: recipe,
     isPending,
     error
   }: UseQueryResult<Recipe> = useQuery({
-    queryKey: ['recipes'],
+    queryKey: ['recipes', id],
     queryFn: () => getRecipeById(id),
     enabled: !!id
   });
@@ -27,19 +32,20 @@ const DetailPage: React.FC= () => {
 
   return (
     <>
-      <article
-        className="bg-red-600 bg-[linear-gradient(to_bottom,_rgba(255,_255,_255,_0)_50%,_#fff)] h-[500px]"
-        style={{ backgroundImage: `url(${recipe?.thumbnail})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
-      >
-        <header className="h-[80px] flex flex-row justify-between items-center px-[2px]">
-          <Link href='/'>
+      <article className="h-[500px] w-full">
+        <header className="px-[24px] absolute left-0 top-0 z-10 w-full h-[80px] flex flex-row justify-between items-center">
+          <Link href="/">
             <IconBackButton />
           </Link>
           {/*TODO: 검색페이지 링크 추가하기*/}
-          <Link href='/'>
+          <Link href="/">
             <IconSearch />
           </Link>
         </header>
+        <div className="relative w-full h-full">
+          <img src={recipe?.thumbnail} alt={recipe?.title} className="w-full h-full object-cover" />
+          <div className="absolute inset-0 bg-[linear-gradient(to_bottom,_rgba(255,_255,_255,_0)_50%,_#fff)]" />
+        </div>
       </article>
       {recipe && <RecipeSection recipe={recipe} />}
     </>
